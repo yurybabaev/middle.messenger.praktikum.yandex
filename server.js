58f8d0ad2d5f9a156c0d6f7ceb8b517ce71b2f7e
Yury Babaev <yurybabaev@yandex.ru>
2022-06-09 17:34:41 +0300

    fix according to review

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
 const express = require('express');
-// const dotnev = require('dotenv');
-// dotnev.config();
+const dotnev = require('dotenv');
+dotnev.config();
 
 const app = express();
-const PORT = /*process.env.PORT ||*/ 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(express.static(`${__dirname}/dist`));
 
