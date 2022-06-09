3d98c1bcd9a97c400a40e2c5773cb9bad8dc0b0b
Yury Babaev <yurybabaev@yandex.ru>
2022-06-09 17:38:54 +0300

    add typed props to link component

--- src/components/link/link.ts
+++ src/components/link/link.ts
@@ -2,8 +2,14 @@ import Block from '../../utils/block';
 import template from './link.hbs';
 import * as classes from './link.module.scss';
 
+export interface ILinkProps {
+  href?: string;
+  caption?: string;
+  classModifier?: string;
+}
+
 export class Link extends Block {
-  constructor(props: any) {
+  constructor(props: ILinkProps) {
     super({ ...props, classes });
   }
 
